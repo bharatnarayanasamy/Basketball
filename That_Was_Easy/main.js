4a132bf0dccc2105782c456535fef96a83cf1402
Bharat Narayanasamy <bharat.narayanasamy@gmail.com>
2017-11-12 00:26:40 +0000

    That Was Easy

---
+++ That_Was_Easy/main.js
@@ -0,0 +1,9 @@
+var thatWasEasy = new Audio("that_was_easy.mp3");
+thatWasEasy.play();
+
+function sayThatWasEasy() {
+   var thatWasEasy = new Audio("that_was_easy.mp3");
+   thatWasEasy.play();
+ }
+ 
+$("#easy").on("click", sayThatWasEasy);
